09bec275e3463f0254bdb2d1ca7fbc7d86af73c9
Saccani Filippo <filippo.saccani@studenti.iispascal.it>
2023-05-02 13:50:20 +0200

    ADD: main e inizio parte view

--- Registro/src/view/Main.java
+++ Registro/src/view/Main.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
-/**
- *
- * @author studente
- */
-public class Main {
+import controller.GestoreRegistro;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        GestoreRegistro gest = new GestoreRegistro();
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Login o Registrazione?");
+        String risposta = scan.nextLine();
+        
+        while (!"Login".equals(risposta) && !"Registrazione".equals(risposta)) {
+            System.out.println(risposta + "\n");
+            System.out.println("Risposta non valida. Login o Registrazione?");
+            risposta = scan.nextLine();
+        } 
+        
+        switch(risposta) {
+            case "Login": {
+                System.out.println("Email: ");
+                String email = scan.nextLine();
+                System.out.println("Password: ");
+                String password = scan.nextLine();
+            }
+            case "Registrazione": {
+                System.out.println("Studente, Insegnante o Dirigente?");
+            }
+        }
     }
-    
 }
